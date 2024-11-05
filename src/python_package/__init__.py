8ff1ebc3f05d02847203ba12ed8e98ab1eab5079
Your Name <you@example.com>
2024-11-05 08:37:11 +0300

    end

--- src/python_package/__init__.py
+++ src/python_package/__init__.py
@@ -3,6 +3,5 @@
 #   Licensed under the MIT License. See LICENSE in project root for information.
 #   -------------------------------------------------------------
 """Python Package Template"""
-from __future__ import annotations
-
-__version__ = "0.0.2"
+import mkBot
+__version__ = "1.0"
